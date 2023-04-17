fec4c2cd4dbdea9273aa41dc3c0b05d5fc4c8702
marcelkawski <marcel.kawski1@gmail.com>
2023-04-17 17:03:01 +0300

    Add reseting game state functionality

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 'use strict';
 
-const drawnNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = drawnNumber;
-let score = 10;
+const initialScore = 10;
+const defaultMessage = document.querySelector('.message').textContent;
+const defaultBackgroundColor =
+  document.querySelector('body').style.backgroundColor;
+const defaultNumberBoxValue = document.querySelector('.number').textContent;
+const defaultNumberBoxWidth = document.querySelector('.number').style.width;
 
-document.querySelector('.score').textContent = score;
+const initializeGame = function () {
+  const drawnNumber = drawNumber();
+  const score = initialScore;
+  return [drawnNumber, score];
+};
+
+const resetGame = function () {
+  drawnNumber = drawNumber();
+  document.querySelector('.number').textContent = defaultNumberBoxValue;
+  score = initialScore;
+
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.message').textContent = defaultMessage;
+  document.querySelector('body').style.backgroundColor = defaultBackgroundColor;
+  document.querySelector('.number').style.width = defaultNumberBoxWidth;
+  document.querySelector('.guess').value = '';
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = document.querySelector('.guess').value;
@@ -17,6 +37,10 @@ document.querySelector('.check').addEventListener('click', function () {
     // guessed
     if (guessNum === drawnNumber) {
       document.querySelector('.message').textContent = 'Correct number! 🥇';
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').textContent = drawnNumber;
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('.score').textContent = score;
     } else {
       score--;
       document.querySelector('.score').textContent = score;
@@ -33,3 +57,15 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+  resetGame();
+});
+
+const drawNumber = () => Math.trunc(Math.random() * 20) + 1;
+
+// let drawnNumber = drawNumber();
+// // document.querySelector('.number').textContent = drawnNumber;
+// let score = initialScore;
+
+let [drawnNumber, score] = initializeGame();
